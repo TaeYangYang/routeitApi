5478f5acada44f8aed0fb1e37927a984b8c0be88
TaeYang <zzzz1031@naver.com>
2025-03-26 14:32:37 +0900

    feat : 예외 공통 포맷 설정 예외 공통 포맷 설정

--- src/main/kotlin/com/routeit/routeitapi/application/base/controller/BaseControllerAdvice.kt
+++ src/main/kotlin/com/routeit/routeitapi/application/base/controller/BaseControllerAdvice.kt
@@ -2,14 +2,15 @@ package com.routeit.routeitapi.application.base.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.routeit.routeitapi.application.base.dto.ResponseDto
+import com.routeit.routeitapi.exception.BaseRuntimeException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.MethodParameter
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
@@ -44,4 +45,21 @@ class BaseControllerAdvice @Autowired constructor(private val objectMapper: Obje
       else -> return ResponseDto(HttpStatus.OK.value(), "SUCCESS", HttpStatus.OK.reasonPhrase, body)
     }
   }
+
+  @ExceptionHandler(Exception::class)
+  private fun exceptionHandler(e: Exception): ResponseDto<String> {
+    e.printStackTrace()
+    return createFailResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.name, e.message ?: "")
+  }
+
+  @ExceptionHandler(BaseRuntimeException::class)
+  private fun baseRuntimeExceptionHandler(e: BaseRuntimeException): ResponseDto<String> {
+    e.printStackTrace()
+    return createFailResponse(e.code, e.status, e.message)
+  }
+
+  private fun createFailResponse(code: Int, status: String, message: String): ResponseDto<String> {
+    return ResponseDto(code, status, message, "")
+  }
+
 }
